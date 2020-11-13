dd7857f18723601d49e78d982cf8936097bfa776
Mr.Leon <levan.melkonian@gmail.com>
2020-11-14 00:22:05 +0200

    host +

--- WebCinema/settings.py
+++ WebCinema/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'gr7)%l9ilx7hf*=bekek%6w47o&v(qqqbf50ms8nul@d7v%w*2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
